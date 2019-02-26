b63d5266d27d724bc7b6b83e918395bbd181e1a2
Laboratório-00-00 <Laboratrio-00-00@192.168.231.26>
2019-02-26 20:06:38 -0300

    Revert "sdds louro juse"
    This reverts commit 2c8237d4bb0c9f3539388fe20b9b06cb3c42abca.

--- testejava/src/testejava/Testejava.java
+++ testejava/src/testejava/Testejava.java
@@ -15,7 +15,7 @@ public class Testejava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello hell/n");
+        // TODO code application logic here
     }
     
 }
