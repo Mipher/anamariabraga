2c8237d4bb0c9f3539388fe20b9b06cb3c42abca
Laboratório-00-00 <Laboratrio-00-00@192.168.231.26>
2019-02-26 20:03:27 -0300

    sdds louro juse

--- testejava/src/testejava/Testejava.java
+++ testejava/src/testejava/Testejava.java
@@ -15,7 +15,7 @@ public class Testejava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello hell/n");
     }
     
 }
